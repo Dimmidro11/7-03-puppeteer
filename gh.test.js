842ba50f3fbe4050fe052959771370dd1973a74d
Dimmidroll <diman15061997@mail.ru>
2025-08-31 19:31:00 +0500

    HW changed

--- gh.test.js
+++ gh.test.js
@@ -14,49 +14,43 @@ describe("Github page tests", () => {
     await page.goto("https://github.com/team");
   });
 
-  test("The h1 header content'", async () => {
-    page.setDefaultTimeout(60000);
+  test("The h1 header content", async () => {
     const firstLink = await page.$("header div div a");
     await firstLink.click();
     await page.waitForSelector('h1');
     const title2 = await page.title();
     expect(title2).toEqual('GitHub · Build and ship software on a single, collaborative platform · GitHub');
-  });
+  }, 60000);
 
   test("The first link attribute", async () => {
-    page.setDefaultTimeout(20000);
     const actual = await page.$eval("a", link => link.getAttribute('href') );
     expect(actual).toEqual("#start-of-content");
-  });
+  }, 20000);
 
   test("The page contains Sign in button", async () => {
-    page.setDefaultTimeout(5000);
     const btnSelector = ".btn-large-mktg.btn-mktg";
     await page.waitForSelector(btnSelector, {
       visible: true,
     });
     const actual = await page.$eval(btnSelector, link => link.textContent.trim());
     expect(actual).toMatch(/^Get started with Team$/)
-  });
+  }, 5000);
 });
 
-  test("The h1 header content'", async () => {
-    page.setDefaultTimeout(5000);
+test("GH AI content", async () => {
     await page.goto("https://github.com/resources/articles/ai");
     const title = await page.title();
     expect(title).toEqual('AI · GitHub');
-  });
+  }, 5000);
 
-    test("The h1 header content'", async () => {
-        page.setDefaultTimeout(5000);
+test("GH Enterprise content", async () => {
     await page.goto("https://github.com/enterprise");
     const title = await page.title();
     expect(title).toEqual('The AI Powered Developer Platform. · GitHub');
-  });
+  }, 5000);
 
-    test("The h1 header content'", async () => {
-        page.setDefaultTimeout(5000);
+  test("GH DecSecOps content", async () => {
     await page.goto("https://github.com/solutions/use-case/devsecops");
     const title = await page.title();
     expect(title).toEqual('Unified DevSecOps Solutions Built for Security | GitHub · GitHub');
-  });
+  }, 5000);
